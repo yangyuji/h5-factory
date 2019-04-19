ec17d18812baa043ecd2abf37e9c3f8060504e0a
yangyuji <394712273@qq.com>
2019-04-19 17:30:23 +0800

    remove node_modules

--- node_modules/es5-ext/.prettierrc.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-module.exports = {
-	printWidth: 100,
-	tabWidth: 4
-};
--- node_modules/vue-loader/lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Plugin } from 'webpack'
-
-declare namespace VueLoader {
-  class VueLoaderPlugin extends Plugin {}
-}
-
-export = VueLoader
